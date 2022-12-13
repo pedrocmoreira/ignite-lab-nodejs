85687791647dd42bf25ddb48547f42bdab6b12a1
Pedro Moreira <pedro.henrique@agenciaalldigital.com>
2022-12-13 20:01:20 -0300

    feat: criando exemplos com mail,para explicação de injeção e inversão de dependencias

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { SMTPMailService } from './mail/smtp-mail.service';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -8,7 +8,7 @@ describe('AppController', () => {
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [SMTPMailService],
     }).compile();
 
     appController = app.get<AppController>(AppController);
